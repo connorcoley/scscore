dc4adf07b97d0d8658acd33c8189895ddc37b426
leswing <lilleswing@gmail.com>
2018-01-30 17:50:52 -0500

    Read from gzip json

--- scscore/standalone_model_numpy.py
+++ scscore/standalone_model_numpy.py
@@ -1,16 +1,19 @@
 '''
 This is a standalone, importable SCScorer model. It does not have tensorflow as a
-dependency and is a more attractive option for deployment. The calculations are 
+dependency and is a more attractive option for deployment. The calculations are
 fast enough that there is no real reason to use GPUs (via tf) instead of CPUs (via np)
 '''
 
 import math, sys, random, os
 import numpy as np
 import time
-import rdkit.Chem as Chem 
+import rdkit.Chem as Chem
 import rdkit.Chem.AllChem as AllChem
+import cPickle as pickle
+import json
+import gzip
 
-import os 
+import os
 project_root = os.path.dirname(os.path.dirname(__file__))
 
 score_scale = 5.0
@@ -29,11 +32,8 @@ class SCScorer():
         self._restored = False
 
     def restore(self, weight_path=os.path.join(project_root, 'models', 'full_reaxys_model_1024bool', 'model.ckpt-10654.as_numpy.pickle'), FP_rad=FP_rad, FP_len=FP_len):
-        self.FP_len = FP_len; self.FP_rad = FP_rad 
-
-        import cPickle as pickle
-        with open(weight_path, 'rb') as fid:
-            self.vars = pickle.load(fid)
+        self.FP_len = FP_len; self.FP_rad = FP_rad
+        self._load_vars(weight_path)
         print('Restored variables from {}'.format(weight_path))
 
         if 'uint8' in weight_path or 'counts' in weight_path:
@@ -43,13 +43,13 @@ class SCScorer():
                 fp = AllChem.GetMorganFingerprint(mol, self.FP_rad, useChirality=True) # uitnsparsevect
                 fp_folded = np.zeros((self.FP_len,), dtype=np.uint8)
                 for k, v in fp.GetNonzeroElements().iteritems():
-                    fp_folded[k % self.FP_len] += v 
+                    fp_folded[k % self.FP_len] += v
                 return np.array(fp_folded)
         else:
             def mol_to_fp(self, mol):
                 if mol is None:
                     return np.zeros((self.FP_len,), dtype=np.float32)
-                return np.array(AllChem.GetMorganFingerprintAsBitVect(mol, self.FP_rad, nBits=self.FP_len, 
+                return np.array(AllChem.GetMorganFingerprintAsBitVect(mol, self.FP_rad, nBits=self.FP_len,
                     useChirality=True), dtype=np.bool)
         self.mol_to_fp = mol_to_fp
 
@@ -67,7 +67,7 @@ class SCScorer():
         # Each pair of vars is a weight and bias term
         for i in range(0, len(self.vars), 2):
             last_layer = (i == len(self.vars)-2)
-            W = self.vars[i] 
+            W = self.vars[i]
             b = self.vars[i+1]
             x = np.matmul(x, W) + b
             if not last_layer:
@@ -93,23 +93,36 @@ class SCScorer():
             smi = ''
         return (smi, cur_score)
 
+    def _load_vars(self, weight_path):
+        if weight_path.endswith('pickle'):
+            with open(weight_path, 'rb') as fid:
+                self.vars = pickle.load(fid)
+                self.vars = [x.tolist() for x in self.vars]
+        elif weight_path.endswith('json.gz'):
+            with gzip.GzipFile(weight_path, 'r') as fin:    # 4. gzip
+                json_bytes = fin.read()                      # 3. bytes (i.e. UTF-8)
+                json_str = json_bytes.decode('utf-8')            # 2. string (i.e. JSON)
+                self.vars = json.loads(json_str)
+                self.vars = [np.array(x) for x in self.vars]
+
+
 if __name__ == '__main__':
-    model = SCScorer()    
-    model.restore(os.path.join(project_root, 'models', 'full_reaxys_model_1024bool', 'model.ckpt-10654.as_numpy.pickle'))
+    model = SCScorer()
+    model.restore(os.path.join(project_root, 'models', 'full_reaxys_model_1024bool', 'model.ckpt-10654.as_numpy.json.gz'))
     smis = ['CCCOCCC', 'CCCNc1ccccc1']
     for smi in smis:
         (smi, sco) = model.get_score_from_smi(smi)
         print('%.4f <--- %s' % (sco, smi))
 
-    model = SCScorer()    
-    model.restore(os.path.join(project_root, 'models', 'full_reaxys_model_2048bool', 'model.ckpt-10654.as_numpy.pickle'), FP_len=2048)
+    model = SCScorer()
+    model.restore(os.path.join(project_root, 'models', 'full_reaxys_model_2048bool', 'model.ckpt-10654.as_numpy.json.gz'), FP_len=2048)
     smis = ['CCCOCCC', 'CCCNc1ccccc1']
     for smi in smis:
         (smi, sco) = model.get_score_from_smi(smi)
         print('%.4f <--- %s' % (sco, smi))
 
-    model = SCScorer()    
-    model.restore(os.path.join(project_root, 'models', 'full_reaxys_model_1024uint8', 'model.ckpt-10654.as_numpy.pickle'))
+    model = SCScorer()
+    model.restore(os.path.join(project_root, 'models', 'full_reaxys_model_1024uint8', 'model.ckpt-10654.as_numpy.json.gz'))
     smis = ['CCCOCCC', 'CCCNc1ccccc1']
     for smi in smis:
         (smi, sco) = model.get_score_from_smi(smi)
